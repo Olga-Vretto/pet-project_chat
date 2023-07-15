c37458d29222b2f9da4dff7fa5c1a396ba2e9b68
Olga-Vretto <olgavretto@outlook.com>
2023-07-15 22:27:12 +0300

    feature/login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import { Box, Button, Container, Grid } from '@mui/material';
+import React from 'react';
 
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <Container>
+      <Grid
+        container
+        style={{ height: window.innerHeight - 50 }}
+        alignItems={'center'}
+        justifyContent={'center'}
+      >
+        <Grid
+          style={{ width: 480, background: 'lightblue' }}
+          container
+          alignItems={'center'}
+          direction={'column'}
+        >
+          <Box p={5}>
+            <Button variant={'outlined'}>Enter with Google</Button>
+          </Box>
+        </Grid>
+      </Grid>
+    </Container>
+  );
+};
 
-export default Login
+export default Login;
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,16 +1,20 @@
 import { AppBar, Box, Button, Toolbar } from '@mui/material';
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { LOGIN_ROUTE } from '../../constants/consts';
 
 const Navbar = () => {
   const user = false;
   return (
     <AppBar color={'transparent'} position='static'>
-      <Toolbar>
+      <Toolbar variant={'dense'}>
         <Box sx={{ flexGrow: 1 }} />
         {user ? (
           <Button variant={'outlined'}>Login</Button>
         ) : (
-          <Button variant={'outlined'}>Back</Button>
+          <NavLink to={LOGIN_ROUTE}>
+            <Button variant={'outlined'}>Back</Button>
+          </NavLink>
         )}
       </Toolbar>
     </AppBar>
