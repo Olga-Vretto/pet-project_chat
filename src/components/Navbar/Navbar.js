913f49ba8e82cf069527d5b60a4bff04dad98afd
Olga-Vretto <olgavretto@outlook.com>
2023-07-15 22:02:22 +0300

    add navbar/material UI

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import { AppBar, Box, Button, Toolbar } from '@mui/material';
+import React from 'react';
 
 const Navbar = () => {
+  const user = false;
   return (
-    <div>Navbar</div>
-  )
-}
+    <AppBar color={'transparent'} position='static'>
+      <Toolbar>
+        <Box sx={{ flexGrow: 1 }} />
+        {user ? (
+          <Button variant={'outlined'}>Login</Button>
+        ) : (
+          <Button variant={'outlined'}>Back</Button>
+        )}
+      </Toolbar>
+    </AppBar>
+  );
+};
 
-export default Navbar
+export default Navbar;
